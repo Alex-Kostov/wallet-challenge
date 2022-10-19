6112a9c7c611916cae8b830335421948a95bf1d4
akostov <alex89773@gmail.com>
2022-10-20 00:49:40 +0300

    adding controller for the login, updating the endpoints ifs

--- src/server.ts
+++ src/server.ts
@@ -1,68 +1,44 @@
 import http from 'http';
 import * as dotenv from "dotenv";
 dotenv.config();
+import { loginController, logout } from  './controllers/auth-controller';
 
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer(async (req, res) => {
 	// Set API endpoints
-	// Login
-	if (req.url === "/auth/login" && req.method === "GET") {
+	const { method, url } = req;
+	if (url === "/auth/login" && method === "POST") {
+		loginController(req, res);
+	} else if (url === "/auth/logout" && method === "GET") {
+		logout(req, res);
+	} else if (url === "/users/balance" && method === "GET") {
 		//response headers
 		res.writeHead(200, { "Content-Type": "application/json" });
 		//set the response
 		res.write("Hi there, This is a Vanilla Node.js API");
 		//end the response
 		res.end();
-	}
-
-	// Logout
-	if (req.url === "/auth/logout" && req.method === "GET") {
+	} else if (url === "/wallet/list" && method === "GET") {
 		//response headers
 		res.writeHead(200, { "Content-Type": "application/json" });
 		//set the response
 		res.write("Hi there, This is a Vanilla Node.js API");
 		//end the response
 		res.end();
-	}
-
-	// Balance
-	if (req.url === "/users/balance" && req.method === "GET") {
+	} else if (url === "/wallet/withdraw" && method === "GET") {
 		//response headers
 		res.writeHead(200, { "Content-Type": "application/json" });
 		//set the response
 		res.write("Hi there, This is a Vanilla Node.js API");
 		//end the response
 		res.end();
-	}
-
-	// Wallet list
-	if (req.url === "/wallet/list" && req.method === "GET") {
-		//response headers
-		res.writeHead(200, { "Content-Type": "application/json" });
-		//set the response
-		res.write("Hi there, This is a Vanilla Node.js API");
-		//end the response
-		res.end();
-	}
-
-	// Wallet Withdraw
-	if (req.url === "/wallet/withdraw" && req.method === "GET") {
-		//response headers
-		res.writeHead(200, { "Content-Type": "application/json" });
-		//set the response
-		res.write("Hi there, This is a Vanilla Node.js API");
-		//end the response
-		res.end();
-	}
-
-	// If no route present
-	else {
+	} else {
 		res.writeHead(404, { "Content-Type": "application/json" });
 		res.end(JSON.stringify({ message: "Route not found" }));
 	}
 });
 
 server.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}`);
+	console.log(`Server listening on port: ${PORT}`);
 });
